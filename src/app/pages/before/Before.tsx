f825101c9130ad370b4f282d7cae831a1ea62ece
Nour-eddine <noureddine@globaletik.com>
2023-10-29 21:42:31 +0100

    update login

--- src/app/pages/before/Before.tsx
+++ src/app/pages/before/Before.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "../../Header";
 import {PATH_AUTH} from "../../routing/paths";
 import {Link} from "react-router-dom";
 import {Notification} from "../../modules/components";
 import {toAbsoluteUrl} from "../../../_theme/helpers";
+import { motion, AnimatePresence } from 'framer-motion';
 
 const images = {
     image1: "https://app.leonardo.ai/img/login-hero-images/Celestial.webp",
@@ -11,7 +12,11 @@ const images = {
     image3: "https://leonardo-cdn.b-cdn.net/wp-content/uploads/2023/07/leosplash-1.png"
 }
 
+
 export default function Before() {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
 
     return (
         <>
@@ -52,39 +57,42 @@ export default function Before() {
                         />
                     </div>
 
-                    <div className="row d-flex justify-content-around align-items-center _px-40  _py-32">
+                    <div className="row d-flex justify-content-around align-items-center _px-12  _py-32">
                         <div className="col-3">
-                            <h3 id="brxe-ytffse" className="brxe-heading txt-center rad-color-full">Cultivate
-                                Originality</h3>
-                            <div id="brxe-ilptes" className="brxe-text-basic txt-center sp-txt">Your imagination, our
+                            <h3 className="gradient-color-full">Cultivate Originality</h3>
+                            <div className="text-white">Your imagination, our
                                 technology. Generate distinctive art with pre-trained AI models or train your own.
                             </div>
                         </div>
                         <div className="col-3">
-                            <h3 id="brxe-ytffse" className="brxe-heading txt-center rad-color-full">Cultivate
-                                Originality</h3>
-                            <div id="brxe-ilptes" className="brxe-text-basic txt-center sp-txt">Your imagination, our
+                            <h3 className="gradient-color-full">Cultivate Originality</h3>
+                            <div className="text-white">Your imagination, our
                                 technology. Generate distinctive art with pre-trained AI models or train your own.
                             </div>
                         </div>
                         <div className="col-3">
-                            <h3 id="brxe-ytffse" className="brxe-heading txt-center rad-color-full">Cultivate
-                                Originality</h3>
-                            <div id="brxe-ilptes" className="brxe-text-basic txt-center sp-txt">Your imagination, our
+                            <h3 className="gradient-color-full">Cultivate Originality</h3>
+                            <div className="text-white">Your imagination, our
                                 technology. Generate distinctive art with pre-trained AI models or train your own.
                             </div>
                         </div>
+
+                    </div>
+
+                    <div className="row ">
+                        <h2 className="col-12 d-flex justify-content-center align-items-center">
+                            <span className="gradient-color-full _pr-2">Leonardo’s</span> Toolkit&nbsp;
+                        </h2>
                     </div>
                 </div>
 
 
-                <Notification svg={'/media/svg/social-logos/dribbble.svg'}
-                              title={"title"}
-                              message={"message"}
-                />
+                {/*<Notification svg={'/media/svg/social-logos/dribbble.svg'}*/}
+                {/*              title={"title"}*/}
+                {/*              message={"message"}*/}
+                {/*/>*/}
 
             </main>
-
 
 
             <footer></footer>
